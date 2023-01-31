4eb14a6b971eb708e73a80e556c15cb1ff4a41b1
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-31 18:05:42 +0530

    async added

--- Program.cs
+++ Program.cs
@@ -5,21 +5,48 @@ using System.Text.RegularExpressions;
 
 class Password
 {
-    const string availableChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+<,>.";
-
+    const string availableChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@#$%^&*()-_=+<,>.";
+    static List<string> passList= new List<string>();
+    const string smalls = "abcdefghijklmnopqrstuvwxyz";
+    const string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const string numbers = "0123456789";
+    const string specials = "~!@#$%^&*()-_=+<,>.";
     const string passRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{20,20}$";
-    int recCount = 0;
+    static int recCount = 0;
     int count = 0;
     static string connectionString;
     static SqlConnection cnn;
-    string  PasswordGenerator(ref Random random,ref StringBuilder result)
+    static int current = 0;
+    static int total = 1000000;
+    static string  PasswordGenerator(ref Random random,ref StringBuilder result)
     {
         result.Clear();
+        int total_special=random.Next(2,5);
+        int total_number = random.Next(2, 5);
+        int total_smalls = random.Next(2, 5);
+        int total_capital = 20 - total_smalls - total_number - total_special;
+        for(int i=0;i<total_capital;i++)
+        {
+            result.Append(capitals[random.Next(capitals.Length)]);
+        }
+        for(int i=0;i<total_special;i++)
+        {
+            result.Append(specials[random.Next(specials.Length)]);
+        }
+        for(int i=0;i<total_number;i++)
+        {
+            result.Append(numbers[random.Next(numbers.Length)]);
+        }
+        for(int i=0;i<total_smalls;i++)
+        {
+            result.Append(smalls[random.Next(smalls.Length)]);
+        }
+        return result.ToString();
+        /*
         for(int i=0;i<20;i++)
         {
             result.Append(availableChars[random.Next(availableChars.Length)]);
         }
-
         if (Regex.IsMatch(result.ToString() , passRegex))
         {
 
@@ -33,7 +60,7 @@ class Password
             //Console.WriteLine("Recursive call");
             //Console.WriteLine(result.ToString());
             return PasswordGenerator(ref random,ref result);
-        }
+        }*/
     }
    
     string PasswordGenerate(ref Random random,ref StringBuilder result)
@@ -84,7 +111,7 @@ class Password
         }
     }
     
-    void DBConnect()
+    static void DBConnect()
     {
         connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Meet  Kerasiya\source\repos\PracticeSession2\PracticeSession2\Database1.mdf"";Integrated Security=True";
         cnn = new SqlConnection(connectionString);
@@ -108,46 +135,74 @@ class Password
             Environment.Exit(1);
         }
     }
-    void AddData(ref Random random,ref StringBuilder sb)
+    static async Task<int> AddDataToDB()
     {
-        string query = "Insert Into PasswordTable (password) VALUES (@pass)";
-        SqlCommand cmd = new SqlCommand(query, cnn);
-        cmd.Parameters.AddWithValue("@pass", this.PasswordGenerator(ref random, ref sb));
-        try
+        await Task.Delay(5000);
+        while(current==passList.Count || passList.Count==0)
         {
-            cmd.ExecuteNonQuery();
+
         }
-        catch (SqlException ex)
+        while(current<passList.Count)
         {
-            if (ex.Number == 2627)
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder();
+            string query = "Insert Into PasswordTable (password) VALUES (@pass)";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cmd.Parameters.AddWithValue("@pass", passList[current++]);
+            //cmd.Parameters.AddWithValue("@pass", this.PasswordGenerator(ref random, ref sb));
+            try
             {
-                this.AddData(ref random,ref sb);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Number == 2627)
+                {
+                    AddDataToDB();
+                }
+            }
+            finally
+            {
+                Console.WriteLine(current);
             }
         }
-        finally
-        {
-            Console.WriteLine(count++);
-        }
+        
+        return 0;
     }
-    static void Main(string[] args)
+    static async Task<int> AddDataToList()
+    {
+        Random random= new Random();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<total;i++)
+        {
+            passList.Add(PasswordGenerator(ref random, ref sb));
+        }
+        return 0;
+    }
+    static async Task Main(string[] args)
     {
         var watch = System.Diagnostics.Stopwatch.StartNew();
-
-        
+        DBConnect();
+        cnn.Open();
         Random random = new Random();
         Password passobj = new Password();
         StringBuilder sb = new StringBuilder();
+        Task listTask=AddDataToList();
+        Task DBTask = AddDataToDB();
+        
 
-        passobj.DBConnect();
-
-        cnn.Open() ;
-        for(int i=0;i<3000000;i++)
-        {
-            passobj.AddData(ref random,ref sb);
-            
-        }
+        //Task listTask = new Task(async()=>await AddDataToList());
+        //Task DBTask = new Task(async() =>
+        //{
+        //    DBConnect();
+        //    cnn.Open();
+        //    await AddDataToDB();
+        //});
+        //listTask.Start();
+        //DBTask.Start();
+        await Task.WhenAll(listTask, DBTask);
         cnn.Close() ;
-        Console.WriteLine("Total number of Recursion needed: "+passobj.recCount);
+        Console.WriteLine("Total number of Recursion needed: "+recCount);
 
         watch.Stop();
         var elapsedMs = watch.ElapsedMilliseconds;
